cd16b8361922d414e66b22644ad6a2cd62b95258
andrewsnelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2020-03-02 18:34:06 -0330

    First setting up players.

--- src/com/group2/gui/SetUpPlayersMenu.java
+++ src/com/group2/gui/SetUpPlayersMenu.java
@@ -1,8 +1,17 @@
 package com.group2.gui;
 
-public class SetUpPlayersMenu {
+import javax.swing.*;
+import java.awt.*;
+
+public class SetUpPlayersMenu extends JFrame {
+    //Instance Variables
+    private Container mainPane;
+    private JButton startNewGameButton, loadSavedGameButton;
+    private JPanel buttonPanel, informationPanel;
+    private JLabel infoMessage;
 
     public SetUpPlayersMenu(){
+        super();
 
     }
 }
--- src/com/group2/physicalgameobjects/Player.java
+++ src/com/group2/physicalgameobjects/Player.java
@@ -1,6 +1,12 @@
+package com.group2.physicalgameobjects;
 
+class Player {
 //INSTANCE VARIABLES
 
 //CONSTRUCTOR
+public Player(){
 
+}
 //METHODS
+
+}
