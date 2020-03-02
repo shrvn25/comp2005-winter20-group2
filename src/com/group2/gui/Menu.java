fc61360a4ec96bcdd517f085d9c461db698e2b94
Andrew Snelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2020-03-02 16:28:19 -0330

    Update Menu.java

--- src/com/group2/gui/Menu.java
+++ src/com/group2/gui/Menu.java
@@ -1,11 +1,1 @@
-package com.group2;
-
-import com.group2.startingstoppinggame.StartMenuGUI;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        StartMenuGUI newStartMenu = new StartMenuGUI();
-    }
-}
+//Add
