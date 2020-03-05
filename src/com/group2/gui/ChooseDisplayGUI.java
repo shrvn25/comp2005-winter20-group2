02a289afc828423b8fcdf315a34d138f2aa2c64e
jfhaque <43616263+jfhaque@users.noreply.github.com>
2020-03-05 00:49:58 -0330

    Update ChooseDisplayGUI.java

--- src/com/group2/gui/ChooseDisplayGUI.java
+++ src/com/group2/gui/ChooseDisplayGUI.java
@@ -2,13 +2,17 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
 
-public class ChooseDisplayGUI extends JFrame {
+public class ChooseDisplayGUI extends JFrame{
     //Instance Variables
     private Container mainPane;
-    private JButton simpleOptionButton, complexOptionButton;
-    private JPanel buttonPanel, informationPanel;
-    private JLabel infoMessage;
+    private JButton startButton;
+    private JPanel buttonPanel, informationPanel, playerPanel, boardPanel;
+    private JLabel infoMessage, difficulty, board,humanPlayer, player1, player2, player3, player4;
+    private JRadioButton simple, complex;
+    private JComboBox<String> playerList,playerList2,playerList3;
 
 
     //Constructor
@@ -17,26 +21,82 @@ public class ChooseDisplayGUI extends JFrame {
 
         mainPane = getContentPane();
         mainPane.setPreferredSize(new Dimension(500,500));
-        mainPane.setLayout(new BorderLayout());
+        mainPane.setLayout(new GridLayout(5,1));
         
-        //Make Buttons
-        simpleOptionButton = new JButton("START NEW GAME");
-        complexOptionButton = new JButton("LOAD SAVED GAME");
+        
 
+        //Board Style
+        simple= new JRadioButton("Simple");
+        complex= new JRadioButton("Complex");
+
+    
+
+        
         //TODO Add listeners to buttons
 
         //Make the information label
         infoMessage = new JLabel("Please choose a simple display or a complex display.");
         informationPanel = new JPanel();
         informationPanel.add(infoMessage);
-        mainPane.add(informationPanel, BorderLayout.NORTH);
+        mainPane.add(informationPanel);
 
+        
+        //Making the board panel
+        boardPanel= new JPanel();
+        board= new JLabel("Board Type");
+
+        boardPanel.add(board);
+        boardPanel.add(simple);
+        boardPanel.add(complex);
+        mainPane.add(boardPanel);
+
+        //Making the player panel
+        
+
+        playerPanel= new JPanel(new GridLayout(6,2));
+        playerList= new JComboBox<String>();
+        playerList.addItem("Human Player");
+        playerList.addItem("Computer Player");
+        
+        playerList2= new JComboBox<String>();
+        playerList2.addItem("Human Player");
+        playerList2.addItem("Computer Player");
+        
+        playerList3= new JComboBox<String>();
+        playerList3.addItem("Human Player");
+        playerList3.addItem("Computer Player");
+
+        playerList.setSelectedIndex(0);
+        playerList2.setSelectedIndex(0);
+        playerList3.setSelectedIndex(0);
+
+        player1= new JLabel("Player 1: ");
+        humanPlayer= new JLabel("Human Player");
+        player2= new JLabel("Player 2: ");
+        player3= new JLabel("Player 3: ");
+        player4= new JLabel("Player 4: ");
+
+        playerPanel.add(player1);
+        playerPanel.add(humanPlayer);
+        playerPanel.add(player2);
+        playerPanel.add(playerList);
+        playerPanel.add(player3);
+        playerPanel.add(playerList2);
+        playerPanel.add(player4);
+        playerPanel.add(playerList3);
+
+        mainPane.add(playerPanel);
+        
+        
+          
+
+        
         //Make the button label
         buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(1, 2));
-        buttonPanel.add(simpleOptionButton);
-        buttonPanel.add(complexOptionButton);
-        mainPane.add(buttonPanel, BorderLayout.SOUTH);
+        startButton= new JButton("Start");
+        
+        buttonPanel.add(startButton);
+        mainPane.add(buttonPanel);
 
 
         //Pack and make visible
