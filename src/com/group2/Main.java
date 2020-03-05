d34359aef04a0eddeb29ae9a5d5abcff4ea1abcd
jfhaque <43616263+jfhaque@users.noreply.github.com>
2020-03-04 21:21:05 -0330

    Add files via upload

---
+++ src/com/group2/Main.java
@@ -0,0 +1,12 @@
+package group2;
+
+import gui.ChooseDisplayGUI;
+import gui.StartMenuGUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StartMenuGUI newStartMenu = new StartMenuGUI();
+    }
+}
