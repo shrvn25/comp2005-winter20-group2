1b68f0fc41669e60564ac14c9f7ef3a25fd2ff86
andrewsnelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2020-03-06 13:15:37 -0330

    Added class and methods.

---
+++ src/com/group2/gui/GameSettings.java
@@ -0,0 +1,29 @@
+package com.group2.gui;
+
+public class GameSettings {
+
+
+
+
+    public void setBoardType(){
+
+    }
+
+    public void setDifficulty(){
+
+    }
+
+    public void setPlayer1PlayerType(){
+
+    }
+    public void setPlayer2PlayerType(){
+
+    }
+    public void setPlayer3PlayerType(){
+
+    }
+    public void setPlayer4PlayerType(){
+
+    }
+
+}
