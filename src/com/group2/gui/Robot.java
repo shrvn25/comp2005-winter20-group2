9f9112070504dff5abb9f93b0fefae3ae93eab9f
andrewsnelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2020-03-07 14:41:09 -0330

    updated Robot class.

--- src/com/group2/gui/Robot.java
+++ src/com/group2/gui/Robot.java
@@ -1,9 +1,33 @@
 package com.group2.gui;
 
-import java.awt.*;
-
 public class Robot {
+    String color;
+    String shape;
 
+    public Robot(String newColor, String newShape){ //TODO this is new now with color and shape inputs
+        this.color = newColor;
+        this.shape = newShape;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setColor(String newColor){
+        this.color = color;
+    }
+
+    public String getShape(){
+        return this.shape;
+    }
+
+    public void setShape(String newShape){
+        this.shape = newShape;
+    }
+
+
+
+/* Originally there
     private static final Color[] robotColours = {
             new Color(245, 99, 99), //Color.RED,
             new Color(99, 245, 99), //Color.GREEN,
@@ -13,4 +37,10 @@ public class Robot {
     public static final String[] robotShapes = {
             "circle", "triangle", "square", "hexagon"
     };
+
+ */
+
+
+
+
 }
