bfdf42d3bc5564e6536593ae9d37f695cf6a945a
andrewsnelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2020-03-07 14:48:06 -0330

    updated SimpleBoard class.

--- src/com/group2/physicalgameobjects/SimpleBoard.java
+++ src/com/group2/physicalgameobjects/SimpleBoard.java
@@ -1,8 +1,20 @@
 package com.group2.physicalgameobjects;
 
-public class SimpleBoard extends GameBoard {
+public class SimpleBoard extends GameBoard { //TODO Check this
+
+    GameBoard[][] newGameBoard;
 
     public SimpleBoard() {
         super();
     }
+
+    public void setTargetSquares(){
+
+    }
+
+    public void setBoundaries(){
+
+    }
+
+
 }
