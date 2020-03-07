2853547056db8447d947c8180be8ed1609295821
andrewsnelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2020-03-07 14:50:24 -0330

    updated ComplexBoard class

--- src/com/group2/physicalgameobjects/ComplexBoard.java
+++ src/com/group2/physicalgameobjects/ComplexBoard.java
@@ -2,8 +2,17 @@ package com.group2.physicalgameobjects;
 
 public class ComplexBoard extends GameBoard {
 
+    GameBoard[][] newGameBoard;
 
     public ComplexBoard(){
         super();
+    }
+
+    public void setTargetSquares(){
+
+    }
+
+    public void setBoundaries(){
+
     }
 }
