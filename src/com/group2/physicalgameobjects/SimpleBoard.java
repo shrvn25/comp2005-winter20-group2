cf57396c0d6705d50ee5fede01dc6c5cdf3d2991
andrewsnelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2020-03-07 21:47:07 -0330

    further updates.

--- src/com/group2/physicalgameobjects/SimpleBoard.java
+++ src/com/group2/physicalgameobjects/SimpleBoard.java
@@ -2,10 +2,15 @@ package com.group2.physicalgameobjects;
 
 public class SimpleBoard extends GameBoard { //TODO Check this
 
-    GameBoard[][] newGameBoard;
+    //int [][][][] newGameBoardAsCoordinates;
+    int [][] newGameBoardAsCoordinates;
+
 
     public SimpleBoard() {
         super();
+        newGameBoardAsCoordinates = new int[16][16][1][1];
+        //Todo remember x and y coordinates in Java
+
     }
 
     public void setTargetSquares(){
--- src/com/group2/physicalgameobjects/Square.java
+++ src/com/group2/physicalgameobjects/Square.java
@@ -1,4 +1,75 @@
 package com.group2.physicalgameobjects;
 
+import java.util.List;
+
 public class Square {
+    int xyCoordinate[][];
+    List listOfItemsOnSquare; //Because you can store different data types in an we need to make a list instead of arraylist(Like barrier and if a robot is on it or not). Designate a index in the array for something.
+    //Robot robotOnSquare;
+    boolean robotIsOnSquare;
+    boolean squareIsATargetTile;
+
+
+    public Square(){
+        //Default is that there is nothing on the square at first, you have to add to it.
+
+        xyCoordinate = new int[-1][-1];//That coordinate doesn't exist and so you need to change it before use.
+        listOfItemsOnSquare = new List;
+
+        for (int i = 0; i < 4; i++) { //Three spaces for barrier in first index, targetsquare in second, and robotsquare in third.
+            //Add null object to all of these now //TODO Is this correct?
+            listOfItemsOnSquare.add(null);
+        }
+
+        robotIsOnSquare = false;
+        squareIsATargetTile = false;
+
+    }
+
+    public addRobotToSquare(Robot newRobot){
+        robotIsOnSquare = true;
+        listOfItemsOnSquare.add(2, newRobot);
+    }
+
+    public boolean isRobotOnSquare(){
+        if (robotIsOnSquare == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public Robot getRobotOnSquare(){
+        Robot robotToReturn = (Robot) listOfItemsOnSquare.get(2);//TODO IS THIS OK????? CASTING HERE????
+        return robotToReturn;
+    }
+
+    public void removeRobotFromSquare(){
+        robotIsOnSquare = false;
+        listOfItemsOnSquare.set(2, null);
+
+    }
+
+    /*public addBarrierToSquare(){
+
+    }*/ //This is later with a complex board.
+
+    public void addTargetSquareToSquare(TargetTile newTargetTile){
+
+        listOfItemsOnSquare.add(1, newTargetTile);
+    }
+
+    public boolean isSquareATargetTile(){
+        if (squareIsATargetTile == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+
+
 }
